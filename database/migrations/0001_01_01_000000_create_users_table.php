4395b9c04ccd6f457f3ada76b7f10b067f9ed851
paaaaayns <ejpines21@gmail.com>
2025-07-09 10:59:50 +0800

    Add profiles migration with user relationship and gender field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->ulid('id')->primary();
+            $table->string('email')->index()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            $table->string('password')->nullable();
+            $table->timestamp('password_changed_at')->nullable();
+
+            $table->softDeletes();
+            $table->foreignUlid(column: 'deleted_by')->nullable()->constrained('users');
+
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_07_09_025911_profiles.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\GenderEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('user_id')->unique()->constrained('users')->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('contact_number');
+            $table->enum('gender', array_map(fn($case) => $case->value, GenderEnum::cases()))->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
