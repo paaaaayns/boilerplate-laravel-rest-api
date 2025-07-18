325ca150a95c5a7b381e38d254d493ac703fe2df
paaaaayns <ejpines21@gmail.com>
2025-07-18 10:10:02 +0800

    Move authentication routes to a new api_v1.php file for better organization and maintainability.

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('auth/spa')->group(function () {
-    Route::get('authenticate', [AuthController::class, 'authenticate']);
-    Route::post('login', [AuthController::class, 'login'])->middleware('guest');
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-});
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
