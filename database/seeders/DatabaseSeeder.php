c69440fbb48a44cb9e6b96f570aa1ac985cd33c2
paaaaayns <ejpines21@gmail.com>
2025-07-18 10:02:18 +0800

    Enhance DatabaseSeeder to create profiles for users with complete details including first name, last name, and contact number

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleEnum;
+use App\Models\Profile;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,81 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+        ]);
+
+        $superAdmin = User::factory()
+            ->create([
+                'email' => 'super.admin@test.com',
+                'password' => Hash::make('Password123!!'),
+            ])->assignRole(RoleEnum::SuperAdmin->value);
+
+        $admin = User::factory()
+            ->create([
+                'email' => 'admin@test.com',
+                'password' => Hash::make('Password123!!'),
+            ])->assignRole(RoleEnum::Admin->value);
+
+
+        $employee = User::factory()
+            ->create([
+                'email' => 'employee@test.com',
+                'password' => Hash::make('Password123!!'),
+            ])->assignRole(RoleEnum::Employee->value);
+
+        $user = User::factory()
+            ->create([
+                'email' => 'user@test.com',
+                'password' => Hash::make('Password123!!'),
+            ])->assignRole(RoleEnum::User->value);
+
+        $superAdmin->profile()->save(
+            Profile::factory()->make(
+                [
+                    'first_name' => 'SUPER',
+                    'last_name' => 'ADMIN',
+                    'middle_name' => null,
+                    'contact_number' => "09999999999",
+                ]
+            )
+        );
+
+        $admin->profile()->save(
+            Profile::factory()->make(
+                [
+                    'first_name' => 'ADMIN',
+                    'last_name' => 'ADMIN',
+                    'middle_name' => null,
+                    'contact_number' => "09999999999",
+                ]
+            )
+        );
+        
+        $employee->profile()->save(
+            Profile::factory()->make(
+                [
+                    'first_name' => 'EMPLOYEE',
+                    'last_name' => 'EMPLOYEE',
+                    'middle_name' => null,
+                    'contact_number' => "09999999999",
+                ]
+            )
+        );
+
+        $user->profile()->save(
+            Profile::factory()->make(
+                [
+                    'first_name' => 'USER',
+                    'last_name' => 'USER',
+                    'middle_name' => null,
+                    'contact_number' => "09999999999",
+                ]
+            )
+        );
+
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
