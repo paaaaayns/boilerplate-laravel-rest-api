5804f41f7f9cba5d12e5478a213e907442c4acfc
paaaaayns <ejpines21@gmail.com>
2025-08-19 11:04:18 +0800

    feat: Add user restoration and profile update routes in API

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::patch('users/{user}/restore', [UserController::class, 'restore'])->name('users.restore');
+Route::patch('users/{user}/profile', [ProfileController::class, 'update']);
+Route::patch('users/{user}/role', [UserController::class, 'updateRole']);
+Route::patch('users/{user}/password', [UserController::class, 'updatePassword']);
+Route::apiResource('users', UserController::class);
 
 Route::prefix('auth/spa')->group(function () {
     Route::get('authenticate', [AuthController::class, 'authenticate']);
