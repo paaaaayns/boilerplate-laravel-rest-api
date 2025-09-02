77eeb319204d7322d3c93ca3ad55165b6acb2c65
paaaaayns <ejpines21@gmail.com>
2025-09-02 10:53:48 +0800

    feat: Add employee_id column to profiles table migration

--- database/migrations/2025_07_09_025911_create_profiles_table.php
+++ database/migrations/2025_07_09_025911_create_profiles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->ulid('id')->primary();
+            $table->string('employee_id')->index()->unique();
             $table->foreignUlid('user_id')->unique()->constrained('users')->cascadeOnDelete();
             $table->string('first_name');
             $table->string('middle_name')->nullable();
