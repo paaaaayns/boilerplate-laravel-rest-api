5bdd5de49c4a8cced0958c7341875965d167ae1a
paaaaayns <ejpines21@gmail.com>
2025-09-05 16:41:51 +0800

    feat: add user import and export routes to API

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -9,6 +9,10 @@ Route::patch('users/{user}/restore', [UserController::class, 'restore'])->name('
 Route::patch('users/{user}/profile', [ProfileController::class, 'update']);
 Route::patch('users/{user}/role', [UserController::class, 'updateRole']);
 Route::patch('users/{user}/password', [UserController::class, 'updatePassword']);
+Route::post('users/import', [UserController::class, 'import'])->name('import');
+Route::get('users/import/template', [UserController::class, 'downloadImportTemplate'])->name('download.import.template');
+Route::get('users/export', [UserController::class, 'export'])->name('export');
+Route::get('users/export/download/{filename}', [UserController::class, 'downloadExportFile'])->name('download.export.file');
 Route::apiResource('users', UserController::class);
 
 Route::prefix('auth/spa')->group(function () {
